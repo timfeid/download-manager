8f6844d7e9ca5c5f16b548af39ac66f27a6d3fae
Tim Feid <tim.feid@roubini.com>
2016-02-26 17:54:24 -0500

    better example index

--- index.js
+++ index.js
@@ -1,29 +1,14 @@
 #!/usr/bin/env node
 
 var manager = require('./lib/manager')
-var args = process.argv.slice(2)
+
+manager.on('download.progress', function (download) {
+  console.log('progress', download.url, download.downloaded / download.contentLength * 100);
+})
 
 manager.on('add', function (download) {
-  console.log('added: ' + download.url)
+  download.start();
 })
 
-manager.on('download.start', function (download) {
-  console.log('starting download for ', download.url)
-})
-
-manager.on('download.init', function (download) {
-  console.log('init download for ', download.url)
-})
-
-manager.on('download.headers', function (download) {
-  console.log('downloading file to ' + download.file)
-})
-
-manager.add(args[0])
-  .on('progress', function (download) {
-    console.log('progress: ', download.downloaded / download.contentLength * 100)
-  })
-  .start()
-  .on('complete', function (download) {
-    console.log('download completed: ', download.file)
-  })
+manager.add('https://getcomposer.org/installer')
+manager.add('https://nodejs.org/dist/v4.3.1/node-v4.3.1.pkg')
