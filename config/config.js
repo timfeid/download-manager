0926f104fe6085198a8c4dba46f40232c933af65
Tim Feid <tim.feid@roubini.com>
2016-02-26 16:32:58 -0500

    a little clean up

--- config/config.js
+++ config/config.js
@@ -2,12 +2,18 @@
 
 var config = module.exports = {}
 
+// where to store our temporary files
 config.tempDir = './tmp/'
+// where to put our downloads
 config.downloadDir = './downloads/'
+// default driver (download entire request in one shot)
 config.defaultDriver = require('./../lib/drivers/single')
 
+// drivers to check before using the default
 config.drivers = []
+// parts driver (download in multiple requests, faster)
 config.drivers.push(require('./../lib/drivers/parts')(8))
 
+// array of authenticatable websites
 config.sites = []
 config.sites.push(require('./sites/uploaded'))
--- config/sites/uploaded.js
+++ config/sites/uploaded.js
@@ -3,7 +3,10 @@
 var env = require('dotenv').config()
   , uploaded = module.exports = {}
 
+// how to tell if the url is a match for this config
 uploaded.match = /uploaded\.net/
+
+// how to authenticate (form url, method, and params)
 uploaded.authenticate = {
   url: 'http://uploaded.net/io/login',
   method: 'POST',
