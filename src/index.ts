440561844e63177fa7bdfc97e12bf4b93688b880
Tim Feid <tim.feid@tellimer.com>
2020-08-30 13:55:39 -0400

    chore: prepare for build

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { Download } from "./download";
 
-const download = new Download('https://timfeid.com/test.txt')
+// const download = new Download('https://timfeid.com/test.txt')
 
-download.on('error', error => console.log(error))
-download.start().then(r => console.log(r))
+// download.on('error', error => console.log(error))
+// download.start().then(r => console.log(r))
+
+export default Download
